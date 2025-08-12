c161687e3c7d463d15b1080d12e0c17b39b5d941
GustavoJava <gustavo.java2000@gmail.com>
2025-08-12 20:00:43 -0300

    mat-toolbar e sidenav-bar

--- UI/front-inventory/src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ UI/front-inventory/src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,3 +1,4 @@
+import { MediaMatcher } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent {
+
+  mobileQuery!: MediaQueryList;
+
+  menuNav = [
+    {name: "Home", route:"home", icon:"home"},
+    {name: "Categorias", route:"category", icon:"category"},
+    {name: "Produtos", route:"product", icon:"production_quantity_limits"},
+  ]
+
+  constructor(media: MediaMatcher){
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  }
 
 }
