3f6443eb88e60672172b817c8b0bc80bbb945b2a
GustavoJava <gustavo.java2000@gmail.com>
2025-08-12 21:53:08 -0300

    corrigindo component home

--- UI/front-inventory/src/app/modules/dashboard/dashboard-routing.module.ts
+++ UI/front-inventory/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-
-
 import { RouterModule, Routes } from '@angular/router';
+
 import { DashboardComponent } from './pages/dashboard.component';
+
 
 const routes: Routes = [
   {
--- UI/front-inventory/src/app/modules/dashboard/dashboard.module.ts
+++ UI/front-inventory/src/app/modules/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './pages/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './pages/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 
 
