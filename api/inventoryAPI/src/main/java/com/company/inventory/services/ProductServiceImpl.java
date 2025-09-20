7190a4e11dc45d964ae5558747a62bbec95ee1a6
GustavoJava <gustavo.java2000@gmail.com>
2025-09-20 16:19:23 -0300

    melhorado o delete by Id

--- api/inventoryAPI/src/main/java/com/company/inventory/services/ProductServiceImpl.java
+++ api/inventoryAPI/src/main/java/com/company/inventory/services/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.company.inventory.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -18,11 +19,11 @@ import com.company.inventory.response.ProductResponseRest;
 import com.company.inventory.util.Util;
 
 @Service
-public class ProductServiceImpl implements IProductService{
-	
+public class ProductServiceImpl implements IProductService {
+
 	private ICategoryDao categoryDao;
 	private IProductDao productDao;
-	
+
 	public ProductServiceImpl(ICategoryDao categoryDao, IProductDao productDao) {
 		super();
 		this.categoryDao = categoryDao;
@@ -35,20 +36,20 @@ public class ProductServiceImpl implements IProductService{
 
 		ProductResponseRest response = new ProductResponseRest();
 		List<Product> list = new ArrayList<>();
-		
+
 		try {
-			
+
 			Optional<Category> category = this.categoryDao.findById(categoryId);
-			
+
 			if (category.isPresent()) {
 				product.setCategory(category.get());
 			} else {
 				response.setMetadata("resposta nok", "-1", "categoria não encontrada");
 				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
 			}
-			
+
 			Product productSaved = productDao.save(product);
-			
+
 			if (Objects.nonNull(productSaved)) {
 				list.add(productSaved);
 				response.getProductResponse().setProducts(list);
@@ -57,27 +58,27 @@ public class ProductServiceImpl implements IProductService{
 				response.setMetadata("resposta nok", "-1", "erro produto não salvo!");
 				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.BAD_REQUEST);
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			response.setMetadata("resposta nok", "-1", "erro produto não salvo!");
 			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
-		
+
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public ResponseEntity<ProductResponseRest> searchById(Long id) {
-		
+
 		ProductResponseRest response = new ProductResponseRest();
 		List<Product> list = new ArrayList<>();
-		
+
 		try {
-			
+
 			Optional<Product> product = this.productDao.findById(id);
-			
+
 			if (product.isPresent()) {
 				byte[] imgDescompressed = Util.decompressZLib(product.get().getPicture());
 				product.get().setPicture(imgDescompressed);
@@ -89,12 +90,12 @@ public class ProductServiceImpl implements IProductService{
 				response.setMetadata("resposta nok", "-1", "produto não encontrado!");
 				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			response.setMetadata("resposta nok", "-1", "erro produto não encontrado!");
 			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 	}
 
 	@Override
@@ -103,20 +104,20 @@ public class ProductServiceImpl implements IProductService{
 		ProductResponseRest response = new ProductResponseRest();
 		List<Product> list = new ArrayList<>();
 		List<Product> listAux = new ArrayList<>();
-		
+
 		try {
-			
+
 			listAux = this.productDao.findByNameContainingIgnoreCase(name);
-			
+
 			if (!listAux.isEmpty()) {
-				
-				listAux.stream().forEach(p ->{
+
+				listAux.stream().forEach(p -> {
 					byte[] imgDescompressed = Util.decompressZLib(p.getPicture());
 					p.setPicture(imgDescompressed);
 					list.add(p);
 				});
-				
-				//list.addAll(listAux);
+
+				// list.addAll(listAux);
 				response.getProductResponse().setProducts(list);
 				response.setMetadata("resposta ok", "00", "produtos encontrados!");
 				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
@@ -124,8 +125,8 @@ public class ProductServiceImpl implements IProductService{
 				response.setMetadata("resposta nok", "-1", "produtos não encontrados!");
 				return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			response.setMetadata("resposta nok", "-1", "erro ao buscar produto!");
 			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -134,21 +135,27 @@ public class ProductServiceImpl implements IProductService{
 	@Override
 	@Transactional
 	public ResponseEntity<ProductResponseRest> deleteById(Long id) {
-		
+
 		ProductResponseRest response = new ProductResponseRest();
-		
+
 		try {
-			
+
+			this.productDao.findById(id).orElseThrow(
+					() -> new NoSuchElementException("Produto não encontrado, erro ao excluir produto com ID: " + id));
+
 			this.productDao.deleteById(id);
+
 			response.setMetadata("resposta ok", "00", "produto eliminado!");
-			
-		}catch(Exception e) {
-			response.setMetadata("resposta nok", "-1", "erro ao excluir produto!");
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
+
+		} catch (NoSuchElementException e) {
+			response.setMetadata("resposta nok", "-1", e.getMessage());
+			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.NOT_FOUND);
+		} catch (Exception e) {
+			response.setMetadata("resposta nok", "-1", "erro ao excluir produto: " + e.getMessage());
 			return new ResponseEntity<ProductResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		return new ResponseEntity<ProductResponseRest>(response, HttpStatus.OK);
-		
+
 	}
 
 }
